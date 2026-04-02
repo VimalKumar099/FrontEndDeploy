23ecf8ba63fc50067eaaf9cc1f04356218ada7d6
VimalKumar099 <2400032827@kluniversity.in>
2026-04-02 10:02:12 +0530

    Update API base URL to remove products endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://backenddeploy-production-4754.up.railway.app/api/products"
+  baseURL: "https://backenddeploy-production-4754.up.railway.app/api"
 });
 
 export default API;
